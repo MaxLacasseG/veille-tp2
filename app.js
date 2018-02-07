9d89eb37668d82fb2cae2b0544cce981c2f846af
Maxime Lacasse Germain <max.lacasse.g@gmail.com>
2018-02-07 09:47:18 -0500

    Yolo

--- app.js
+++ app.js
@@ -1,28 +1,42 @@
-/*jshint esversion: 6 */ 
+/*jshint esversion: 6 */
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const fs = require('fs');
+const enregistreMembres = (data)=>{
 
+}
 app.set('view engine', 'ejs');
 
-app.use(express.static(__dirname+'/assets/'));
+app.use(express.static(__dirname + '/assets/'));
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.render('index');
 });
 
-app.get('/membres', (req,res)=>{
+app.get('/membres', (req, res) => {
     res.render('membres');
 });
 
-app.get('/formulaire', (req,res)=>{
+app.get('/formulaire', (req, res) => {
     res.render('formulaire');
 });
 
-app.post('/formulaire', (req,res)=>{
+app.get('/traiter_form', (req, res) => {
+    reponse = {
+        prenom: req.query.prenom,
+        nom: req.query.nom,
+        tel: req.query.tel,
+        courriel: req.query.courriel
+    };
+    console.log(reponse);
+    res.end(JSON.stringify(reponse));
+});
+
+app.post('/formulaire', (req, res) => {
     res.render('formulaire');
 });
 
-app.listen(8000, ()=>{
+app.listen(8000, () => {
     console.log('Écoute sur port 8000');
 });
