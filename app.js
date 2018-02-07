1b869cfb501793f7d3c9a7734fd8f7cae8350bd1
Maxime Lacasse Germain <max.lacasse.g@gmail.com>
2018-02-07 09:14:25 -0500

    Ajout des vues pour les différentes routes

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+/*jshint esversion: 6 */ 
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
 app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname+'/assets/'));
 
 app.get('/', (req,res)=>{
     res.render('index');
@@ -12,7 +15,11 @@ app.get('/membres', (req,res)=>{
     res.render('membres');
 });
 
-app.post('/formulaire', bodyParser.urlencoded, (req,res)=>{
+app.get('/formulaire', (req,res)=>{
+    res.render('formulaire');
+});
+
+app.post('/formulaire', (req,res)=>{
     res.render('formulaire');
 });
 
