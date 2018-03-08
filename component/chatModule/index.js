42d829aa858bbc3a90b6b75ea1ea49d2ddd56c2f
Maxime Lacasse Germain <max.lacasse.g@gmail.com>
2018-03-07 21:41:47 -0500

    Fonction de déconnection

--- component/chatModule/index.js
+++ component/chatModule/index.js
@@ -2,18 +2,36 @@ let socket = require('socket.io');
 
 module.exports.listen = function (server) {
     let io = socket.listen(server);
-
+    io.membres = [];
     // GESTION DU CHAT
     io.on('connection', function (socket) {
         socket.on('enregistrement', function (data) {
             data.id = socket.id;
-            io.emit('nouvelUtilisateur', data);
+            socket.nom = data.nom;
+            socket.broadcast.emit('nouvelUtilisateur', data);
         });
 
-        socket.on('nouveauMessage', function(data){
-            console.log(data.nom, data.message);
+        socket.on('demanderNoms', function () {
+            var tabNoms = [];
+            Object.keys(io.sockets.connected).forEach(function (socketID) {
+                var util = {}
+                util.id = io.sockets.connected[socketID].id;
+                util.nom = io.sockets.connected[socketID].nom;
+                if (util) tabNoms.push(util);
+            });
+
+            socket.emit('recevoirListeNoms', tabNoms);
+        });
+
+        socket.on('nouveauMessage', function (data) {
+            //console.log(data.nom, data.message);
             socket.broadcast.emit('recevoirMessage', data);
-        })
+        });
+
+        socket.on('disconnect', function (data) {
+            //console.log(io.membres);
+            socket.broadcast.emit('enleverUtil', socket.id);
+        });
     });
 
 
--- public/js/chat.js
+++ public/js/chat.js
@@ -2,8 +2,6 @@
 var Client = {}
 
 const connexionChat= ()=>{
-    let btnConnexionChat = document.querySelector(".btnConnexionConteneur button");
-    btnConnexionChat.parentNode.style.display = "none";
     //CONNEXION
     Client = io.connect();
     Client.on('connect', () => {
@@ -19,20 +17,43 @@ const connexionChat= ()=>{
         afficherNom(data.nom, data.id)
     });
 
+    Client.on('recevoirListeNoms', (data)=>{
+        recevoirListeNoms(data);
+    })
+
     //RÉCEPTION D'UN NOUVEAU MESSAGE
     Client.on('recevoirMessage', (data) => {
         recevoirMsg(data)
+    });
+
+    //DÉCONNEXION D'UN AUTRE UTILISATEUR
+    Client.on('enleverUtil', (data) => {
+        enleverUtil(data);
     });
 };
 
 
 const connecterUtil = () => {
     Client.nom = document.querySelector('input.nomChat').value;
+    
+    document.querySelector(".btnConnexionConteneur").style.display = "none";
+    let btnDeconnexion = document.querySelector(".btnDeconnexionConteneur").style.display = "block";
+    document.getElementsByTagName('span')[0].innerText = Client.nom;
+
     Client.emit('enregistrement', {
         nom: Client.nom
     });
+
+    Client.emit('demanderNoms');
 }
 
+const recevoirListeNoms = (liste)=>{
+    console.log(liste);
+    for(let membre of liste){
+        console.log("membre", membre)
+        afficherNom(membre.nom, membre.id);
+    }
+}
 const afficherNom = (nom, id) => {
     let liste = document.querySelector('.listeChat');
     let cloneListeNom = document.querySelector('.listeChat .membreChat.gabarit');
@@ -43,7 +64,6 @@ const afficherNom = (nom, id) => {
     liste.appendChild(nouvelUtil);
 }
 const envoyerMsg = () => {
-    console.log("ok")
     let message = document.querySelector(".chatBoiteMsg").value;
     let msgGabarit = document.querySelector('.chatBoiteRep .chatRep.auteur');
     let msgClone = msgGabarit.cloneNode(true);
@@ -57,6 +77,7 @@ const envoyerMsg = () => {
         nom: Client.nom,
         message: message
     });
+    document.querySelector(".chatBoiteMsg").value = "";
 }
 
 const recevoirMsg = (data)=>{
@@ -67,4 +88,32 @@ const recevoirMsg = (data)=>{
     msgClone.querySelector('.msg').innerText = data.message;
     msgClone.querySelector('.ami').innerText = data.nom;
     msgGabarit.parentNode.appendChild(msgClone);
+}
+
+const deconnexionChat = ()=>{
+    Client.disconnect();
+
+   let liste = document.querySelectorAll('ul.listeChat li:not(.gabarit)');
+   viderListe(liste);
+   let messages = document.querySelectorAll('.chatBoiteRep div:not(.gabarit)')
+   viderListe(messages);
+
+    let btnDeconnexion = document.querySelector(".btnDeconnexionConteneur").style.display = "none";
+    document.getElementsByTagName('span')[0].innerText = "";
+    document.querySelector('input.nomChat').value = "";
+    document.querySelector(".btnConnexionConteneur").style.display = "block";
+
+}
+
+const enleverUtil = (data)=>{
+    console.log('deconnect', data)
+    let utilisateur = document.querySelector('ul.listeChat li[data-id="'+data+'"]');
+    console.log(utilisateur);
+    utilisateur.parentNode.removeChild(utilisateur);
+}
+
+const viderListe = (liste)=>{
+    for(let elm of liste) {
+        elm.parentNode.removeChild(elm);
+    };
 }
